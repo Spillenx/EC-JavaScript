40b6000ad9da7463a65af68574204a35c54ad4b9
spillenx <spillenx@gmail.com>
2020-10-28 00:46:52 +0100

    cleaning up the MVC

--- JSAssignment/controller/controller.js
+++ JSAssignment/controller/controller.js
@@ -16,7 +16,8 @@ class Controller {
     }
 
     currenciesLoad() {
-        model.currenciesLoad()
+        let url = this.currenciesUrl();
+        model.currenciesLoad(url)
         .then(response => response.json())
         .then(function(data) {   
             currencies = data;
@@ -26,7 +27,8 @@ class Controller {
     }
 
     weatherLoad() {
-        model.weatherLoad()
+        let url = this. weatherURL();
+        model.weatherLoad(url)
         .then(response => response.json())
         .then(function(data) {   
             weather = data;
@@ -35,4 +37,36 @@ class Controller {
         .catch(error => alert(error))
     }
 
+        // genereates and returns url forexchangerate API
+        currenciesUrl() {
+
+            // key = 86ac07a01d57484529525c6c
+            // https://v6.exchangerate-api.com/v6/YOUR-API-KEY/latest/SEK
+    
+            let url_BASE = "https://v6.exchangerate-api.com/v6/";
+            let url_KEY = "86ac07a01d57484529525c6c";
+            let url_CURRENCY = "/latest/SEK";
+    
+            let url = url_BASE + url_KEY + url_CURRENCY;
+            return url;
+        }
+    
+        // generates and returns url for openweather API with ID from all cities in countries.json
+        weatherURL() {
+    
+            // key = 976ba469ccfcae3180d6448ff913c400
+            // api.openweathermap.org/data/2.5/group?id={id,..,id}&appid={API key}
+    
+            let url_BASE = 'htps://api.openweathermap.org/data/2.5/group?id=';
+            let url_KEYS = '&appid=976ba469ccfcae3180d6448ff913c400';
+            let url_UNIT = '&units=metric';
+            // let url_LANG = '&lang=SE';
+    
+            for(let i = 0; i < countries.length; i++) {
+                url_BASE += countries[i].id + ',';
+            }
+            url_BASE = url_BASE.substring(0, url_BASE.length -1);
+            let url = url_BASE + url_KEYS + url_UNIT;
+            return url;
+        }
 }
--- JSAssignment/model/model.js
+++ JSAssignment/model/model.js
@@ -1,43 +1,26 @@
 class Model {
 
-    // fetch countries from local file countries.json
+    // fetches countries from local file countries.json
     async countriesLoad() {
         let promise = await fetch('json/countries.json');
         return promise;
     }
 
-    // fetch currencies from exchangerate-api.com
-    async currenciesLoad() {
-        let promise = await fetch('');
-        return promise;
-    }
-
-    // fetch weather data from openweathermap.org
-    async weatherLoad() {
-        let url = this.weatherURL();
-        //url = url.substring(0, url.length);
+    // fetches currencies from exchangerate-api.com
+    async currenciesLoad(url) {
+        //let url = this.currenciesUrl();
         console.log(url);
         let promise = await fetch(url);
         return promise;
     }
 
-    // generates url for openweather API with ID from all cities in countries.json
-    weatherURL() {
-
-        // ow_API_Key = 976ba469ccfcae3180d6448ff913c400
-        // api.openweathermap.org/data/2.5/group?id={id,..,id}&appid={API key}
-
-        let url_BASE = 'htps://api.openweathermap.org/data/2.5/group?id=';
-        let url_KEYS = '&appid=976ba469ccfcae3180d6448ff913c400';
-        let url_UNIT = '&units=metric';
-        // let url_LANG = '&lang=SE';
-
-        for(let i = 0; i < countries.length; i++) {
-            url_BASE += countries[i].id + ',';
-        }
-        let url = url_BASE + url_KEYS + url_UNIT;
-        //console.log(url);
-        return url;
+    // fetches weather data from openweathermap.org
+    async weatherLoad(url) {
+        //let url = this.weatherURL();
+        //url = url.substring(0, url.length);
+        console.log(url);
+        let promise = await fetch(url);
+        return promise;
     }
-
+    
 }
