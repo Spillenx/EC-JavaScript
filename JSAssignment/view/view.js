ece58547c4ee91b1ff5a41c1a21571d1c00f6751
spillenx <spillenx@gmail.com>
2020-10-28 12:19:24 +0100

    Added city selecter and cleaned up MVC

--- JSAssignment/view/view.js
+++ JSAssignment/view/view.js
@@ -1,10 +1,26 @@
 class View {
 
-    
-    countriesShow(countries) {
-        for(let i = 0; i < countries.length; i++) {
-            //alert(countries[i].city);
-        }
+    selectCity() {
+        let cityList = '';
+        for (let i=0; i<countries.length; i++) {
+            cityList += '<option value="' + i + '">' + countries[i].city + '</option>';
+        }   
+        Helper.setHtml('city-list', cityList);
+    }
+
+    showCity() {
+
     }
     
+    showWeather() {
+        //"http://openweathermap.org/img/wn/" + icon + "@2x.png"
+        let html = '';
+
+        for(let i = 0; i < weather.list.length; i++) {
+            html += '<img src="http://openweathermap.org/img/wn/' + 'weather.list[i].weather[0].icon' + '@2x.png">';
+        }
+
+        Helper.setHtml('selected-weather', html);
+        Helper.show('selected-weather');
+    }
 }
