e7f816ad5a591e2bb51df96c67ab1dca72d66e3c
spillenx <spillenx@gmail.com>
2020-11-04 23:48:42 +0100

    Layout/design

--- JSAssignment/view/view.js
+++ JSAssignment/view/view.js
@@ -20,8 +20,6 @@ class View {
 
     // show selected city
     showCity(cityIndex) {
-        Helper.clearInput('exchange-amount');
-        Helper.setHtml('recieved-amount', '');
 
         let htmlCity = '';
         htmlCity += 'Country: ' + countries[cityIndex].country + '<br>';
@@ -32,6 +30,9 @@ class View {
         Helper.setHtml('current-currency', countries[cityIndex].currency);
 
         controller.weatherLoad(cityIndex);
+
+        Helper.clearInput('exchange-amount');
+        Helper.setHtml('recieved-amount', '');
 
         Helper.show('city-container');
         Helper.show('weather-container');
@@ -70,7 +71,7 @@ class View {
                 htmlWeather += '<div class="weather-body">';
                     htmlWeather += '<div class="weather-info">';
                     htmlWeather += 'Weather: ' + data.list[i].weather[0].description + '<br>';
-                    htmlWeather += 'Temperature : ' + data.list[i].main.temp.toFixed(1) + '&#8451;<br>';
+                    htmlWeather += 'Temperature: ' + data.list[i].main.temp.toFixed(1) + '&#8451;<br>';
                     //htmlWeather += 'Date and time: ' + data.list[i].dt_txt;
                 htmlWeather += '</div>';
                 htmlWeather += '<img src="http://openweathermap.org/img/wn/' + data.list[i].weather[0].icon + '@2x.png">';
